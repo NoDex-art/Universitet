14db0fa71aa6897f6962c9d90e16df15c4dd2e95
NoDex-art <maksimus20060923@gmail.com>
2025-06-02 04:22:29 +0300

    Create main.cpp

---
+++ lb№10/main.cpp
@@ -0,0 +1,14 @@
+#include "TTime.h"
+#include "Expression.h"
+#include "BooleanData.h"
+
+// Основна функція програми
+int main() {
+    // Виклик функції для завдання Param77
+    task_param77();
+    // Виклик функції для завдання Begin25
+    task_begin25();
+    // Виклик функції для завдання Boolean5
+    task_boolean5();
+    return 0;
+}
