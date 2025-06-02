cce98104b4a8f403becefe186d35a474c21463f2
NoDex-art <maksimus20060923@gmail.com>
2025-06-02 05:21:16 +0300

    Update begin.cpp

--- lb№10/begin.cpp
+++ lb№10/begin.cpp
@@ -1,27 +1,8 @@
 #include "Expression.h"
 #include <iostream>
 #include <cmath>
-#include <limits>
 
 using namespace std;
-
-// Функція для перевірки коректності float
-bool isValidFloat(float x) {
-    return !isnan(x) && !isinf(x);
-}
-
-// Функція для введення та перевірки виразу
-bool inputAndValidateExpression(Expression& e) {
-    cout << "Введіть x: ";
-    cin >> e.x;
-    if (cin.fail() || !isValidFloat(e.x)) {
-        cout << "Некоректне введення float!" << endl;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        return false;
-    }
-    return true;
-}
 
 // Функція для обчислення виразу y = 3x^6 - 6x^2 - 7
 void computeExpression(Expression& e) {
@@ -31,18 +12,10 @@ void computeExpression(Expression& e) {
 // Функція для виконання завдання Begin25
 void task_begin25() {
     cout << "\n--- Begin25: Обчислення y = 3x^6 - 6x^2 - 7 ---" << endl;
-    // Попередньо визначені тестові випадки
-    float testXs[5] = {0.0, 1.0, -1.0, 2.0, -2.0};
-    for (int i = 0; i < 5; i++) {
-        Expression expr;
-        expr.x = testXs[i];
-        computeExpression(expr);
-        cout << "Тест " << i + 1 << ": x = " << expr.x << ", y = " << expr.y << endl;
-    }
-    // Введення користувача
     Expression expr;
-    if (inputAndValidateExpression(expr)) {
-        computeExpression(expr);
-        cout << "Введення користувача: x = " << expr.x << ", y = " << expr.y << endl;
-    }
+    cout << "Введіть x: ";
+    cin >> expr.x;
+    
+    computeExpression(expr);
+    cout << "x = " << expr.x << ", y = " << expr.y << endl;
 }
